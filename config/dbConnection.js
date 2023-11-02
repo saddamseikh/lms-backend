b38f0348da832dc10c239442cd24372e81eed8d9
saddamseikh <saddamseikh2229@gmail.com>
2023-11-02 19:14:00 +0530

    database connect (mongodb)

---
+++ config/dbConnection.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+mongoose.set('strictQuery', false);
+
+const connectionToDB = async () => {
+    try {
+        const { connection } = await mongoose.connect(
+            process.env.MONGO_URI || `mongodb://localhost:27017/lms`
+        );
+        if (connection) {
+            console.log(`Connected to mongoDB:${connection.host}`)
+        }
+    }
+    catch (e) {
+        console.log(e)
+        process.exit(1);
+    }
+}
+
+export default connectionToDB;
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import { config } from "dotenv";
 config();
 
 import app from './app.js';
+import connectionToDB from "./config/dbConnection.js";
 
 //port listening 
 const PORT = process.env.PORT ||5010;
-app.listen(PORT,()=>{
-
+app.listen(PORT, async ()=>{
+    // Connect to DB
+    await connectionToDB();
     console.log(`App is running at http://localhost:${PORT} `);
 })
